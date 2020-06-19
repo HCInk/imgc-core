d44222f99802c3b78aec7f95ce9e513e7475c9f2
XLP <1st@xlp-now.de>
2020-06-19 22:28:38 +0200

    Renamed test-files

--- test.sh
+++ test.sh
@@ -10,10 +10,10 @@ if [ "$1" == "no-container" ]; then
 	mkdir -p test_files/one test_files/two test_files/three test_files/four test_files/five
 	../build/imgc-tests
 	if [ "$2" == "generate" ]; then
-    mkdir ../results
-		node ../assets/test.js -a generate -f ../results/out.json -r test_files
+    	mkdir ../results
+		node ../assets/test.js -a generate -f ../results/test-solutions.json -r test_files
 	else
-		node ../assets/test.js -f ../assets/out.json -r test_files
+		node ../assets/test.js -f ../assets/test-solutions.json -r test_files
 	fi
 
 else
@@ -21,9 +21,13 @@ else
 	docker build --build-arg build_hash="$(date)" -t hcink/imgc-tests:latest -f .ci/Dockerfiles/Local .
 #	rm -rf .ci/docker-wd
 	if [ "$1" == "generate" ]; then
-  	docker run -it hcink/imgc-tests:latest sh -c './test.sh no-container generate > /dev/null 2>&1 && cat results/out.json' > generated.json
+		echo "Generating solutions..."
+		docker run -it hcink/imgc-tests:latest sh -c './test.sh no-container generate > /dev/null 2>&1 && cat results/test-solutions.json' > assets/test-solutions.json
+		echo "Generation of solutions finished."
 	else
-		docker run -it hcink/imgc-tests:latest sh -c './test.sh no-container > /dev/null 2>&1 && cat test-run/results/result.xml' > result.xml
+		echo "Testing..."
+		docker run -it hcink/imgc-tests:latest sh -c './test.sh no-container > /dev/null 2>&1 && cat test-run/results/result.xml' > test-result.xml
+		echo "Testing finished."
 	fi
 
 fi
