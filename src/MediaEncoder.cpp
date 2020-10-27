b85db292e1c9308af24bad9620e6eb4deed3a3ae
XLP <1st@xlp-now.de>
2020-10-27 14:01:50 +0100

    MediaEncoder: Use FileBuilder from torasu

--- src/MediaEncoder.cpp
+++ src/MediaEncoder.cpp
@@ -8,81 +8,12 @@
 
 namespace {
 
+typedef torasu::tstd::Dfile::FileBuilder FileBuilder;
+
 inline std::string avErrStr(int errNum) {
 	char error[AV_ERROR_MAX_STRING_SIZE];
 	return std::string(av_make_error_string(error, AV_ERROR_MAX_STRING_SIZE, errNum));
 }
-
-class FileBuilder {
-private:
-	size_t size = 0;
-	std::map<size_t, std::pair<uint8_t*, size_t>, std::less<size_t>> buffers;
-
-public:
-	size_t pos = 0;
-
-	~FileBuilder() {
-		for (auto& buff : buffers) {
-			delete[] buff.second.first;
-		}
-	}
-
-	inline void write(uint8_t* data, size_t dataSize) {
-		if (size == pos) {
-			uint8_t* newBuf = new uint8_t[dataSize];
-			std::copy(data, data+dataSize, newBuf);
-			buffers.insert( std::pair<size_t, std::pair<uint8_t*, size_t>>(pos, std::pair<uint8_t*, size_t>(newBuf, dataSize)));
-			size += dataSize;
-			std::cout << " F:: APPEND " << dataSize << " @ " << pos << " (new size " << size << ")" << std::endl;  
-			pos = size;
-		} else {
-			auto found = buffers.upper_bound(pos);
-			found--;
-			while (dataSize > 0) {
-				if (found != buffers.end()) {
-					std::cout << " F:: FOUND EXISTING @" << found->first << " (" << found->second.second << ") "
-						"- SEARCHED " << pos << " (" << dataSize << " pending)" << std::endl;
-					uint8_t* buffer = found->second.first;
-					size_t bufferOffset = pos - found->first;
-					size_t bufferLeft = found->second.second-bufferOffset;
-					size_t toWrite = std::min(dataSize, bufferLeft);
-					std::cout << " F:: .. WRITE O" << bufferOffset << " W" << toWrite << " (" << bufferLeft << ")" << std::endl;
-
-					std::copy(data, data+toWrite, buffer+bufferOffset);
-					
-					data += toWrite;
-					dataSize -= toWrite;
-					pos += toWrite;
-					found++;
-
-				} else {
-					write(data, dataSize);
-					dataSize = 0;
-				}
-			}
-		}
-	}
-
-	inline torasu::tstd::Dfile* compile() {
-		auto* file = new torasu::tstd::Dfile(size);
-		
-		auto* data = file->getFileData();
-
-		for (auto& buff : buffers) {
-			std::copy(buff.second.first, buff.second.first+buff.second.second, data+buff.first);
-		}
-
-		return file;
-	}
-
-
-	inline void printMap() {
-		for (auto& buffer : buffers) {
-			std::cout << " -> " << buffer.first << " (size " << buffer.second.second << ")" << std::endl;
-		}
-	}
-
-};
 
 struct IOOpaque {
 	// std::ofstream* ofstream;
